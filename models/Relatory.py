932871a620b6328f096aacec060da0cf496ff13b
Julio Moreira <moreira.julio@proton.me>
2025-07-29 15:48:45 -0300

    :construction: Create base for future relatory model

---
+++ models/Relatory.py
@@ -0,0 +1,16 @@
+from models.DataBaseManager import DatabaseManager
+from datetime import datetime
+
+class Relatory:
+    def __init__(self, db: DatabaseManager):
+        self.db = db
+
+    def generatePDF(self):
+        pass
+
+    def _generateQuery(self):
+        pass
+
+    def printPDF(self):
+        pass
+    
